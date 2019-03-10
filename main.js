974ad63a0f3ccaca0d88bd84b97422dcfa2fe83d
Do Le Bao Nguyen <nathan.dole@gmail.com>
2019-03-10 09:05:50 +0700

    Add quotes and icons

--- main.js
+++ main.js
@@ -2,26 +2,27 @@ $(document).ready(function() {
   new WOW().init();
   // intro
   $('#main-author').hide();
-  var mainIntroOptions = {
-    strings: [
-      "The only way it gets better for you is when you get better.</br>^2000 Better is not something you wish, it's something you become.",
-      "Second line"
-    ],
-    fadeOut: true,
-    shuffle: true,
-    typeSpeed: 60,
-    startDelay: 3,
-    preStringTyped: function(arrPos, self) {
-      if (arrPos>0)
-        $('#main-author').fadeOut();
-    },
-    onStringTyped: function(arrPos, self) {
-      $('#main-author').html('Jim Rohn').fadeIn();
+  $.getJSON("quotes.json", function(data){
+    var quotes = $.map(data, function(val) {return val.quote;})
+    var mainIntroOptions = {
+      strings: quotes,
+      fadeOut: true,
+      shuffle: true,
+      typeSpeed: 60,
+      backDelay: 10000,
+      loop: true,
+      preStringTyped: function(arrPos, self) {
+        $('#main-author').hide();
+      },
+      onStringTyped: function(arrPos, self) {
+        var idx = self.sequence[arrPos]
+        $('#main-author').html(data[idx].author).fadeIn();
+      }
     }
-  }
-  if ($('#main-intro').length) {
-    new Typed("#main-intro", mainIntroOptions);
-  }
+    if ($('#main-intro').length) {
+      new Typed("#main-intro", mainIntroOptions);
+    }
+  });
   // toggle
   $('.toggler').click(function(e){
     var el = e.currentTarget,
